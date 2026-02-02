4b7a89f8a393296e1c2e9775f6377f29c2d266f7
Martin Liu <martin.liu@ucalgary.ca>
2026-02-02 11:16:41 -0700

    created about section

--- src/lib/components/about/AboutSection.svelte
+++ src/lib/components/about/AboutSection.svelte
@@ -1,1 +1,16 @@
-<section id="about" class="min-h-screen scroll-m-24"></section>
+<section
+	id="about"
+	class="flex min-h-[calc(100svh-var(--header-h))] w-full
+         scroll-mt-(--header-h) flex-col justify-evenly"
+>
+	<div>
+		<p class="text-8xl font-semibold">Martin Liu</p>
+		<p class="text-2xl text-text-muted">Software Engineering Student</p>
+	</div>
+	<p class="max-w-3xl text-xl leading-relaxed text-text-muted">
+		I’m a Software Engineering student at the University of Calgary with a strong interest in mobile
+		and backend development. Outside of coursework, I’ve been teaching myself Flutter to explore
+		mobile development, and I enjoy experimenting with different software architectures. I’m always
+		learning and building personal projects to continuously improve my skills.
+	</p>
+</section>
