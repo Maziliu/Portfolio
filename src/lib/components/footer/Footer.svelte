16c3011443c97a57c830293e0e71ab866bdb783e
Martin Liu <martin.liu@ucalgary.ca>
2026-02-02 12:47:12 -0700

    created footer

---
+++ src/lib/components/footer/Footer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Footer, FooterIcon } from 'flowbite-svelte';
+	import { LinkedinSolid, GithubSolid } from 'flowbite-svelte-icons';
+</script>
+
+<Footer footerType="socialmedia">
+	<hr class="my-6 border-bg-front sm:mx-auto lg:my-8" />
+	<div class="sm:flex sm:items-center sm:justify-between">
+		<div class="w-full text-text-muted">
+			<p>
+				This site was partially designed in <a href="https://www.figma.com"><b>Figma</b></a> and
+				built using <b>SvelteKit</b>,
+				<a href="https://tailwindcss.com"><b>TailwindCSS</b></a>, and
+				<a href="https://flowbite-svelte.com/"><b>Flowbite</b></a>
+			</p>
+		</div>
+		<div class="mt-4 flex space-x-2 sm:mt-0 sm:justify-center rtl:space-x-reverse">
+			<FooterIcon href="/">
+				<LinkedinSolid
+					class="h-7 w-7 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+				/>
+			</FooterIcon>
+			<FooterIcon href="https://github.com/Maziliu/Portfolio">
+				<GithubSolid
+					class="h-7 w-7 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+				/>
+			</FooterIcon>
+		</div>
+	</div>
+</Footer>
